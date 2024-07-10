939e9404d85da44ea636959cabafac5db098a00c
Bobby1122223 <fadilmuhammad1177@gmail.com>
2024-07-10 18:39:55 +0700

    main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiReplit:
+    'https://3768c82c-ff63-46c9-953d-909bf545316c-00-3ouzewcosnq2w.pike.replit.dev/',
 };
 
 /*
